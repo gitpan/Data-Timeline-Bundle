ff1086b7e6c5f8356b37497402ecb5aabe8146a6
Marcel Grünauer <marcel@cpan.org>
2007-11-19 13:51:14 +0000

    Import of MARCEL/Data-Timeline-Bundle-0.01 from CPAN.
    gitpan-cpan-distribution: Data-Timeline-Bundle
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MARCEL/Data-Timeline-Bundle-0.01.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

---
+++ lib/Data/Timeline/Bundle.pm
@@ -0,0 +1,77 @@
+package Data::Timeline::Bundle;
+
+use strict;
+use warnings;
+
+
+our $VERSION = '0.01';
+
+
+1;
+
+
+__END__
+
+
+
+=head1 NAME
+
+Data::Timeline::Bundle - install Data::Timeline and derived distributions
+
+=head1 SYNOPSIS
+
+    cpan Data::Timeline::Bundle
+
+=head1 DESCRIPTION
+
+This bundle contains L<Data::Timeline> and all related distributions.
+
+=head1 CONTENTS
+
+Data::Timeline
+
+Data::Timeline::IScrobbler
+
+Data::Timeline::SVK
+
+=head1 TAGS
+
+If you talk about this module in blogs, on del.icio.us or anywhere else,
+please use the C<datatimelinebundle> tag.
+
+=head1 VERSION 
+                   
+This document describes version 0.01 of L<Data::Timeline::Bundle>.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<<bug-data-timeline-bundle@rt.cpan.org>>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+
+=head1 AUTHOR
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 by Marcel GrE<uuml>nauer
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+
+=cut
+
